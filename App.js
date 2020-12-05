ad395b3c16bdafd4b9e0573c6071f9052f7537e5
Zikriye Urkmez Cengiz <zikriye.urkmez@navlungo.com>
2020-12-05 18:36:41 +0300

    init project

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  componentDidMount() {
+    console.log("hello");
+    console.log("ziko");
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Ionicons name="ios-pizza" size={100} color="red" />
+        {/* <Text>Hola I am here.</Text> */}
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
