8fb55e809d29014866cd7e1ae180b6ba5ddd58fd
Zikriye Urkmez Cengiz <zikriye.urkmez@navlungo.com>
2020-12-05 19:19:57 +0300

    add firdst component to project

--- App.js
+++ App.js
@@ -2,28 +2,13 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-
+import AddEntry from "./components/AddEntry";
 export default class App extends React.Component {
-  componentDidMount() {
-    console.log("hello");
-    console.log("ziko");
-  }
   render() {
     return (
-      <View style={styles.container}>
-        <Ionicons name="ios-pizza" size={100} color="red" />
-        {/* <Text>Hola I am here.</Text> */}
-        <StatusBar style="auto" />
+      <View>
+        <AddEntry />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
