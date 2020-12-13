8219566c2333df7ba1b41f44ac27a02887b454f6
Zikriye Urkmez Cengiz <zikriye.urkmez@navlungo.com>
2020-12-13 15:07:46 +0300

    add Metric card

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,16 +1,53 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+
+import { connect } from "react-redux";
+import { timeToString, getDailyReminderValue } from "../utils/helpers";
+import MetricCard from "./MetricCard";
+import { white } from "../utils/colors";
 
 class EntryDetail extends Component {
+  componentDidMount() {
+    this.setTitle(this.props.route.params.entryId);
+  }
+  setTitle = (entryId) => {
+    if (!entryId) return;
+
+    const year = entryId.slice(0, 4);
+    const month = entryId.slice(5, 7);
+    const day = entryId.slice(8);
+
+    this.props.navigation.setOptions({
+      title: `${month}/${day}/${year}`,
+    });
+  };
+
   render() {
+    const { entryId } = this.props.route.params;
+    const { metrics } = this.props;
     return (
       <View>
-        <Text>
-          Entry Detail - {JSON.stringify(this.props.route.params.entryId)}
-        </Text>
+        <MetricCard metrics={metrics} />
+        <Text>Entry Detail - {JSON.stringify(entryId)}</Text>
       </View>
     );
   }
 }
 
-export default EntryDetail;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: white,
+    padding: 15,
+  },
+});
+
+function mapStateToProps(state, { route }) {
+  const { entryId } = route.params;
+  return {
+    entryId,
+    metrics: state[entryId],
+  };
+}
+
+export default connect(mapStateToProps)(EntryDetail);
--- components/MetricCard.js
+++ components/MetricCard.js
@@ -8,25 +8,26 @@ export default function MetricCard({ date, metrics }) {
   return (
     <View>
       {date && <DateHeader date={date} />}
-      {Object.keys(metrics).map((metric) => {
-        const {
-          getIcon,
-          displayName,
-          unit,
-          backgroundColor,
-        } = getMetricMetaInfo(metric);
-        return (
-          <View style={styles.metric} key={metric}>
-            {getIcon()}
-            <View>
-              <Text style={{ fontSize: 20 }}>{displayName}</Text>
-              <Text style={{ fontSize: 16, color: gray }}>
-                {metrics[metric]} {unit}
-              </Text>
+      {!!metrics &&
+        Object.keys(metrics).map((metric) => {
+          const {
+            getIcon,
+            displayName,
+            unit,
+            backgroundColor,
+          } = getMetricMetaInfo(metric);
+          return (
+            <View style={styles.metric} key={metric}>
+              {getIcon()}
+              <View>
+                <Text style={{ fontSize: 20 }}>{displayName}</Text>
+                <Text style={{ fontSize: 16, color: gray }}>
+                  {metrics[metric]} {unit}
+                </Text>
+              </View>
             </View>
-          </View>
-        );
-      })}
+          );
+        })}
     </View>
   );
 }
